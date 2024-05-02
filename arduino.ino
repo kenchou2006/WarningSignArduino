12f5755836675629e1c56c55838a9b97e4753c38
zhiz97289 <153784846+zhiz97289@users.noreply.github.com>
2024-05-02 10:27:19 +0800

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -9,26 +9,17 @@ int keyIndex = 0;
 
 int status = WL_IDLE_STATUS;
 
-char server[] = "warningsign.pp.ua";    // name address for Google (using DNS)
+char server[] = "warningsign.pp.ua";
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
-/* -------------------------------------------------------------------------- */
 void setup() {
-/* -------------------------------------------------------------------------- */  
-  //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
   }
-  
-  // check for the WiFi module:
+
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
     while (true);
   }
   
@@ -36,12 +27,10 @@ void setup() {
   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
     Serial.println("Please upgrade the firmware");
   }
-  
-  // attempt to connect to WiFi network:
+
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
      
     // wait 10 seconds for connection:
@@ -51,7 +40,7 @@ void setup() {
   printWifiStatus();
  
   Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
+
   if (client.connect(server, 443)) {
     Serial.println("connected to server");
     // Make a HTTP request:
@@ -63,14 +52,11 @@ void setup() {
 }
 
 void read_response() {
-/* -------------------------------------------------------------------------- */  
+
   uint32_t received_data_num = 0;
   while (client.available()) {
-    /* actual data reception */
     char c = client.read();
-    /* print data to serial port */
     Serial.print(c);
-    /* wrap data to 80 columns*/
     received_data_num++;
     if(received_data_num % 80 == 0) { 
       Serial.println();
@@ -78,12 +64,9 @@ void read_response() {
   }  
 }
 
-/* -------------------------------------------------------------------------- */
-void loop() {
-/* -------------------------------------------------------------------------- */  
+void loop() {  
   read_response();
 
-  // if the server's disconnected, stop the client:
   if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting from server.");
@@ -94,19 +77,12 @@ void loop() {
   }
 }
 
-/* -------------------------------------------------------------------------- */
 void printWifiStatus() {
-/* -------------------------------------------------------------------------- */  
-  // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
-
-  // print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-
-  // print the received signal strength:
   long rssi = WiFi.RSSI();
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
